7848f4fc4555673493c5d31290bd4a764d342a90
unknown <ibnu.setia23@gmail.com>
2022-07-01 22:36:27 +0800

    main

--- application/controllers/admin/transaksi.php
+++ application/controllers/admin/transaksi.php
@@ -303,7 +303,7 @@ defined('BASEPATH') OR exit ('No direct scrip access allowed');
 
 		public function cetak($idTransaksi=null){
 			if(!isset($idTransaksi)) show_404();
-			require_once(APPPATH.'third_party/fpdf/fpdf.php');
+			require_once(APPPATH.'third_party/fpdf/Fpdf.php');
 
 			$idTransaksi = str_replace(['-','_','~'],['=','+','/'],$idTransaksi);
 			$idTransaksi = $this->encryption->decrypt($idTransaksi);
