de4c6041f9e3f81742f6ac672619833eb30f650b
unknown <ibnu.setia23@gmail.com>
2022-07-01 21:57:14 +0800

    main

--- application/controllers/chat.php
+++ application/controllers/chat.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class chat extends CI_Controller {
+class Chat extends CI_Controller {
 
 	var $folder = 'chat/';
 	var $layout = 'chat/layout';
